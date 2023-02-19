560c2504813b19a43826528cb5996c236b87b38d
Rachel Hamby <tnthreat33@gmail.com>
2023-02-19 16:20:44 -0500

    add delete button

--- src/Roster.js
+++ src/Roster.js
@@ -1,17 +1,26 @@
-import React from 'react'
+function Roster({ players, handleRemovePlayer }) {
+  function handleDeleteClick(player) {
+    fetch(`http://localhost:3000/players/${player.id}`, {
+      method: "DELETE",
+    });
+    handleRemovePlayer(player.id);
+  }
 
-
-function Roster ({players}){
-  
-    return (
-    players.length > 0 && players.map(player =>
-      <div key = {player.id}>
-      
-      <h4>{player.name}  #{player.jersey}</h4>
-      <li> Position: {player.position}</li>
-      <li> Class: {player.class}</li>
-      </div>)
-  )
+  return (
+    players.length > 0 &&
+    players.map((player) => (
+      <div key={player.id}>
+        <h4>
+          {player.name} #{player.jersey}
+        </h4>
+        <li>Position: {player.position}</li>
+        <li>Class: {player.class}</li>
+        <button onClick={() => handleDeleteClick(player)}>
+          🗑
+        </button>
+      </div>
+    ))
+  );
 }
 
-export default Roster
+export default Roster;
