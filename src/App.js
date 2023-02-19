271efd6dc03035d2b28d376ffb5195c3c7f4ad28
Rachel Hamby <tnthreat33@gmail.com>
2023-02-19 16:21:19 -0500

    handleDelete function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import React,{useState, useEffect} from 'react'
+import Roster from "./Roster.js"
+import Home from "./Home.js"
+import { Route, Switch, NavLink } from "react-router-dom";
+import NavBar from './NavBar';
+
 
 function App() {
-  const [players, setPlayers]= useState({})
+  const [players, setPlayers]= useState([])
+
   useEffect(() => {
     fetch("http://localhost:3000/players")
       .then((r) => r.json())
       .then(setPlayers);
   }, []);
-  
+
+  function handleRemovePlayer(id) {
+    const newPlayerList = players.filter((player) => player.id !== id);
+    setPlayers(newPlayerList);
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-      
-      </header>
-    </div>
+    <div>
+      <NavBar/>
+   
+    <Switch>
+      <Route path = "/roster">
+        <Roster players={players} handleRemovePlayer={handleRemovePlayer}/>
+      </Route>
+      <Route exact path = "/">
+        <Home players={players} setPlayers={setPlayers}/>
+      </Route>
+     </Switch>
+     </div>
   );
 }
 
