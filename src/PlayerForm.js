cd46b76dd24dbbedc88194f86902576aa8a5292c
Rachel Hamby <tnthreat33@gmail.com>
2023-02-20 19:43:37 -0500

    clear form

--- src/PlayerForm.js
+++ src/PlayerForm.js
@@ -1,16 +1,18 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-function PlayerForm({players, setPlayers}) {
-    const [form, setForm] = useState({});
-  let handleChange = (e) => {
-    let name = e.target.name;
-    let value = e.target.value;
+function PlayerForm({ players, setPlayers }) {
+  const [form, setForm] = useState({});
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
     setForm({
       ...form,
       [name]: value,
     });
   };
-  let handleSubmit = (e) => {
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     fetch("http://localhost:3000/players", {
       method: "POST",
@@ -20,21 +22,44 @@ function PlayerForm({players, setPlayers}) {
       body: JSON.stringify(form),
     })
       .then((res) => res.json())
-      .then((data) => setPlayers([data, ...players]));
+      .then((data) => {
+        setPlayers([data, ...players]);
+        setForm({});
+      });
   };
+
   return (
-    <div>
+    <div className= "form">
       <h2>New Player</h2>
-      <form onSubmit= {handleSubmit}>
-        <input onChange= {handleChange} type="text" name="name" placeholder="Player name" />
-        <input onChange= {handleChange} type="text" name="position" placeholder="Position" />
-        <input onChange= {handleChange} type="text" name="jersey"  placeholder="Jersey Number" />
-        <input onChange= {handleChange} type="text" name="class" placeholder="Class" />
-        <button onChange= {handleChange} type="submit">Add Player</button>
+      <form onSubmit={handleSubmit} >
+        <input
+          onChange={handleChange}
+          type="text"
+          name="name"
+          placeholder="Player name"
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          name="position"
+          placeholder="Position"
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          name="jersey"
+          placeholder="Jersey Number"
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          name="class"
+          placeholder="Class"
+        />
+        <button type="submit">Add Player</button>
       </form>
     </div>
   );
-  console.log(players)
 }
 
 export default PlayerForm;
